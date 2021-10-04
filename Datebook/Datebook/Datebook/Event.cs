984643349bd4a3ffa16cabfa584a005b2f96e41e
Vsubmariner <66769186+Vsubmariner@users.noreply.github.com>
2021-10-04 18:32:54 +0300

    Work started. Added Event class

---
+++ Datebook/Datebook/Datebook/Event.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datebook
+{
+    public class Event
+    {
+        private Guid eventId;
+        private DateTime eventDate;
+        private TimeSpan eventDuration;
+        private string eventDescription;
+        private string eventPlace;
+        private DateTime entryCreationDate;
+
+
+        public Event(DateTime date, TimeSpan duration, string description, string place)
+        {
+            eventId = Guid.NewGuid();
+            this.eventDate = date;
+            this.eventDuration = duration;
+            this.eventDescription = description;
+            this.eventPlace = place;
+            entryCreationDate = DateTime.Now;
+            
+        }
+    }
+}
